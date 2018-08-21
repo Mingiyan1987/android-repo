f180a49932abd0a3b73c6279c594bf7c0c7f1a00
Mingiyan1987 <basanov.mt87@gmail.com>
2018-08-21 08:38:20 +0300

    готово к ДЗ

--- core/src/ru/geekbrains/stargame/screen/MenuScreen.java
+++ core/src/ru/geekbrains/stargame/screen/MenuScreen.java
@@ -5,19 +5,36 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
+import ru.geekbrains.stargame.base.ActionListener;
 import ru.geekbrains.stargame.base.Base2DScreen;
+import ru.geekbrains.stargame.math.Rect;
+import ru.geekbrains.stargame.screen.menu.ButtomNewGame;
+import ru.geekbrains.stargame.screen.menu.ButtonExit;
+import ru.geekbrains.stargame.screen.sprites.Background;
+import ru.geekbrains.stargame.screen.sprites.Star;
 
 /*
     * Экран меню
  */
 
-public class MenuScreen extends Base2DScreen {
+public class MenuScreen extends Base2DScreen implements ActionListener {
 
-    SpriteBatch batch;
-    Texture img;
-    Texture imgBackGround;
+    private static final int STAR_COUNT = 256;
+
+    private static final float BUTTON_PRESS_SCALE = 0.9f;
+    private static final float BUTTON_HEIGHT = 0.15f;
+
+    private Background background;
+    private Texture bgTexture;
+    private TextureAtlas atlas;
+    private Star star[];
+    private ButtonExit buttonExit;
+    private ButtomNewGame buttomNewGame;
+
 
     public MenuScreen(Game game) {
         super(game);
@@ -26,50 +43,84 @@ public class MenuScreen extends Base2DScreen {
     @Override
     public void show() {
         super.show();
-        batch = new SpriteBatch();
-        batch.getProjectionMatrix().idt();
-        imgBackGround = new Texture("background.jpg");
-        img = new Texture("badlogic.jpg");
+        bgTexture = new Texture("textures/bg.png");
+        background = new Background(new TextureRegion(bgTexture));
+        atlas = new TextureAtlas("textures/menuAtlas.tpack");
+        star = new Star[STAR_COUNT];
+        for (int i = 0; i < star.length; i++) {
+            star[i] = new Star(atlas);
+        }
+        buttonExit = new ButtonExit(atlas, this, BUTTON_PRESS_SCALE);
+        buttonExit.setHeightProportion(BUTTON_HEIGHT);
+        buttomNewGame = new ButtomNewGame(atlas, this, BUTTON_PRESS_SCALE);
+        buttomNewGame.setHeightProportion(BUTTON_HEIGHT);
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
+        update(delta);
+        draw();
+    }
+
+    public void draw() {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         batch.begin();
-        batch.draw(imgBackGround, 0, 0);
-        batch.draw(img, -1f, -1f, 2f, 2f);
+        background.draw(batch);
+        for (int i = 0; i < star.length; i++) {
+            star[i].draw(batch);
+        }
+        buttonExit.draw(batch);
+        buttomNewGame.draw(batch);
         batch.end();
+    }
+
+    public void update(float delta) {
+        for (int i = 0; i < star.length; i++) {
+            star[i].update(delta);
+        }
     }
 
     @Override
     public void dispose() {
         super.dispose();
-        batch.dispose();
-        img.dispose();
+        bgTexture.dispose();
+        atlas.dispose();
     }
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        super.touchDown(screenX, screenY, pointer, button);
-        return false;
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
+        for (int i = 0; i < star.length; i++) {
+            star[i].resize(worldBounds);
+        }
+        buttonExit.resize(worldBounds);
+        buttomNewGame.resize(worldBounds);
     }
 
     @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        super.touchUp(screenX, screenY, pointer, button);
-        return false;
+    public boolean touchDown(Vector2 touch, int pointer) {
+        buttonExit.touchDown(touch, pointer);
+        buttomNewGame.touchDown(touch, pointer);
+        return super.touchDown(touch, pointer);
     }
 
     @Override
-    public boolean keyDown(int keycode) {
-        return super.keyDown(keycode);
+    public boolean touchUp(Vector2 touch, int pointer) {
+        buttonExit.touchUp(touch, pointer);
+        buttomNewGame.touchUp(touch, pointer);
+        return super.touchUp(touch, pointer);
     }
 
     @Override
-    public boolean keyUp(int keycode) {
-        return super.keyUp(keycode);
+    public void actionPerformed(Object src) {
+        if (src == buttonExit) {
+            Gdx.app.exit();
+        } else if (src == buttomNewGame) {
+            game.setScreen(new GameScreen(game));
+        }
     }
 }
